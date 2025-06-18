b9b53dd39f865254134af26206ca88f00c111685
Khishigbayar Unursaikhan <khishigbayar.u@gmail.com>
2025-06-18 22:46:36 +0800

    Daalgawar 5 - Tseg garaar burtgeh

--- routes/ajiltanRoute.js
+++ routes/ajiltanRoute.js
@@ -1,32 +1,33 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   ajiltanTatya,
   ajiltanZagvarAvya,
   tsegTatya,
   tsegZagvarAvya,
-} = require("../controller/ajiltanController");
-const excel = require("exceljs");
-const { crud, UstsanBarimt, tokenShalgakh, khuudaslalt } = require("zevback");
-const multer = require("multer");
-const mimetype = require("mime");
+  tsegGaraarBurtgeh,
+} = require('../controller/ajiltanController');
+const excel = require('exceljs');
+const { crud, UstsanBarimt, tokenShalgakh, khuudaslalt } = require('zevback');
+const multer = require('multer');
+const mimetype = require('mime');
 const storage = multer.memoryStorage();
-const Ajiltan = require("../models/ajiltan");
-const Tsol = require("../models/tsol");
-const Tasag = require("../models/tasag");
-const Tuluvluguu = require("../models/tuluvluguu");
-const Kheltes = require("../models/kheltes");
-const Zagvar = require("../models/zagvar");
-const Zurchil = require("../models/zurchil");
-const ZurchliinTurul = require("../models/zurchliinTurul");
-const jwt = require("jsonwebtoken");
-const fs = require("fs");
-const Tseg = require("../models/tseg");
+const Ajiltan = require('../models/ajiltan');
+const Tsol = require('../models/tsol');
+const Tasag = require('../models/tasag');
+const Tuluvluguu = require('../models/tuluvluguu');
+const Kheltes = require('../models/kheltes');
+const Zagvar = require('../models/zagvar');
+const Zurchil = require('../models/zurchil');
+const ZurchliinTurul = require('../models/zurchliinTurul');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const Tseg = require('../models/tseg');
 const filter = (req, file, cb) => {
   if (
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/png"
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/png'
   )
     cb(null, true);
   else cb(null, false);
@@ -39,16 +40,16 @@ const uploadFile = multer({
   storage: storage,
 });
 
-crud(router, "ajiltan", Ajiltan, UstsanBarimt);
-crud(router, "tsol", Tsol, UstsanBarimt);
-crud(router, "kheltes", Kheltes, UstsanBarimt);
-crud(router, "tasag", Tasag, UstsanBarimt);
-crud(router, "zagvar", Zagvar, UstsanBarimt);
-crud(router, "tuluvluguu", Tuluvluguu, UstsanBarimt);
-crud(router, "zurchil", Zurchil, UstsanBarimt);
-crud(router, "zurchliinTurul", ZurchliinTurul, UstsanBarimt);
+crud(router, 'ajiltan', Ajiltan, UstsanBarimt);
+crud(router, 'tsol', Tsol, UstsanBarimt);
+crud(router, 'kheltes', Kheltes, UstsanBarimt);
+crud(router, 'tasag', Tasag, UstsanBarimt);
+crud(router, 'zagvar', Zagvar, UstsanBarimt);
+crud(router, 'tuluvluguu', Tuluvluguu, UstsanBarimt);
+crud(router, 'zurchil', Zurchil, UstsanBarimt);
+crud(router, 'zurchliinTurul', ZurchliinTurul, UstsanBarimt);
 
-router.get("/ajiltanIdgaarAvya/:id", async (req, res, next) => {
+router.get('/ajiltanIdgaarAvya/:id', async (req, res, next) => {
   try {
     var asuult = await Ajiltan.findById(req.params.id);
     res.send(asuult);
@@ -56,26 +57,27 @@ router.get("/ajiltanIdgaarAvya/:id", async (req, res, next) => {
     next(error);
   }
 });
-router.get("/ajiltanZagvarAvya", ajiltanZagvarAvya);
-router.post("/ajiltanTatya", uploadFile.single("file"), ajiltanTatya);
+router.get('/ajiltanZagvarAvya', ajiltanZagvarAvya);
+router.post('/ajiltanTatya', uploadFile.single('file'), ajiltanTatya);
 
-router.get("/tsegZagvarAvya", tsegZagvarAvya);
-router.post("/tsegTatya", uploadFile.single("file"), tsegTatya);
+router.get('/tsegZagvarAvya', tsegZagvarAvya);
+router.post('/tsegTatya', uploadFile.single('file'), tsegTatya);
+router.post('/tsegGaraarBurtgeh', tsegGaraarBurtgeh);
 
-router.post("/ajiltanNevtrey", async (req, res, next) => {
+router.post('/ajiltanNevtrey', async (req, res, next) => {
   try {
     const ajiltan = await Ajiltan.findOne()
-      .where("nevtrekhNer")
+      .where('nevtrekhNer')
       .equals(req.body.nevtrekhNer)
-      .select("+nuutsUg")
+      .select('+nuutsUg')
       .catch((err) => {
         next(err);
       });
     console.log(ajiltan);
     if (!ajiltan)
-      throw new Error("Хэрэглэгчийн нэр эсвэл нууц үг буруу байна!");
+      throw new Error('Хэрэглэгчийн нэр эсвэл нууц үг буруу байна!');
     var ok = await ajiltan.passwordShalgaya(req.body.nuutsUg);
-    if (!ok) throw new Error("Хэрэглэгчийн нэр эсвэл нууц үг буруу байна!");
+    if (!ok) throw new Error('Хэрэглэгчийн нэр эсвэл нууц үг буруу байна!');
 
     const jwt = await ajiltan.tokenUusgeye();
     var butsaakhObject = {
@@ -89,20 +91,20 @@ router.post("/ajiltanNevtrey", async (req, res, next) => {
   }
 });
 
-router.post("/nuutsUgSoliyo/:id", tokenShalgakh, async (req, res, next) => {
+router.post('/nuutsUgSoliyo/:id', tokenShalgakh, async (req, res, next) => {
   try {
     const ajiltan = await Ajiltan.findById(req.params.id);
     ajiltan.isNew = false;
     ajiltan.nuutsUg = req.body.nuutsUg;
     await ajiltan.save();
-    res.send("Amjilttai");
+    res.send('Amjilttai');
   } catch (err) {
     next(err);
   }
 });
 
 router.post(
-  "/tuluvluguuIdevkhuuljye",
+  '/tuluvluguuIdevkhuuljye',
   tokenShalgakh,
   async (req, res, next) => {
     try {
@@ -115,14 +117,14 @@ router.post(
         { _id: tuluvluguuId },
         { idevkhiteiEsekh: true }
       );
-      res.send("Amjilttai");
+      res.send('Amjilttai');
     } catch (err) {
       next(err);
     }
   }
 );
 router.get(
-  "/idevkhiteiTuluvluguuAvya",
+  '/idevkhiteiTuluvluguuAvya',
   tokenShalgakh,
   async (req, res, next) => {
     try {
@@ -134,12 +136,12 @@ router.get(
   }
 );
 
-router.post("/tokenoorAjiltanAvya", (req, res, next) => {
+router.post('/tokenoorAjiltanAvya', (req, res, next) => {
   try {
     if (!req.headers.authorization) {
-      throw new Error("Энэ үйлдлийг хийх эрх байхгүй байна!", 401);
+      throw new Error('Энэ үйлдлийг хийх эрх байхгүй байна!', 401);
     }
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization.split(' ')[1];
     const tokenObject = jwt.verify(token, process.env.APP_SECRET, 401);
     console.log(tokenObject);
     Ajiltan.findById(tokenObject.id)
@@ -148,7 +150,7 @@ router.post("/tokenoorAjiltanAvya", (req, res, next) => {
         res.send(urdunJson);
       })
       .catch((err) => {
-        console.log("aldaa");
+        console.log('aldaa');
         next(err);
       });
   } catch (error) {
@@ -156,17 +158,17 @@ router.post("/tokenoorAjiltanAvya", (req, res, next) => {
   }
 });
 
-router.post("/zogsokhTsegAvya", tokenShalgakh, async (req, res, next) => {
+router.post('/zogsokhTsegAvya', tokenShalgakh, async (req, res, next) => {
   try {
     var idevkhiteiTuluvluguu = await Tuluvluguu.findOne({
       idevkhiteiEsekh: true,
     });
-    console.log("idevkhiteiTuluvluguu", idevkhiteiTuluvluguu);
-    var ObjectId = require("mongodb").ObjectId;
-    console.log("idevkhiteiTuluvluguu", req.body.ajiltniiId);
+    console.log('idevkhiteiTuluvluguu', idevkhiteiTuluvluguu);
+    var ObjectId = require('mongodb').ObjectId;
+    console.log('idevkhiteiTuluvluguu', req.body.ajiltniiId);
     var tseg = await Tseg.findOne({
       tuluvluguuniiId: idevkhiteiTuluvluguu._id,
-      "ajiltnuud._id": new ObjectId(req.body.nevtersenAjiltniiToken.id),
+      'ajiltnuud._id': new ObjectId(req.body.nevtersenAjiltniiToken.id),
     });
     res.send(tseg);
   } catch (error) {
@@ -175,7 +177,7 @@ router.post("/zogsokhTsegAvya", tokenShalgakh, async (req, res, next) => {
 });
 
 router.get(
-  "/zoogdooguiAjiltniiJagsaaltAvya",
+  '/zoogdooguiAjiltniiJagsaaltAvya',
   tokenShalgakh,
   async (req, res, next) => {
     try {
@@ -190,36 +192,36 @@ router.get(
       var idevkhiteiTuluvluguu = await Tuluvluguu.findOne({
         idevkhiteiEsekh: true,
       });
-      var ObjectId = require("mongodb").ObjectId;
+      var ObjectId = require('mongodb').ObjectId;
       var ajiltniiIdnuud = await Tseg.aggregate([
         {
           $match: {
             tuluvluguuniiId: idevkhiteiTuluvluguu._id.toString(),
-            "ajiltnuud.0": {
+            'ajiltnuud.0': {
               $exists: true,
             },
           },
         },
         {
-          $unwind: "$ajiltnuud",
+          $unwind: '$ajiltnuud',
         },
         {
           $project: {
             id: {
-              $toString: "$ajiltnuud._id",
+              $toString: '$ajiltnuud._id',
             },
           },
         },
       ]);
-      console.log("ajiltniiIdnuud", ajiltniiIdnuud);
+      console.log('ajiltniiIdnuud', ajiltniiIdnuud);
       var idnuud = [];
       if (ajiltniiIdnuud && ajiltniiIdnuud.length > 0) {
         ajiltniiIdnuud.forEach((x) => idnuud.push(ObjectId(x.id)));
       }
-      console.log("idnuud", idnuud);
+      console.log('idnuud', idnuud);
 
       if (body?.query) {
-        body.query["_id"] = {
+        body.query['_id'] = {
           $nin: idnuud,
         };
       } else
